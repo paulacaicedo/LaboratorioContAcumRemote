6e8551f4a4bad53d16ffde828222088214d22a2e
paulacaicedo <paula.caicedo.2019@upb.edu.co>
2019-03-08 10:24:11 -0500

    mi primer commit

---
+++ primos.py
@@ -0,0 +1,11 @@
+
+
+#PROGRAMA PRIMO
+
+x=int(input("Introduzca un Numero: "))
+y=int(input("Introduzca un Numero: "))
+
+if x%y==0:
+    print("Es Divisible exacto")
+else:
+    print("No son divisibles exacto")
